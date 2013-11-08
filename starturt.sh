05835adbc5378e4e5279ef526d44df1b77d3c0cd
fly <bsd@voltaire.sh>
2013-11-08 18:39:08 -0500

    no tty

--- starturt.sh
+++ starturt.sh
@@ -4,7 +4,7 @@ SCRIPTPATH=$( builtin cd $(dirname $0) ; pwd -P )
 
 while true
 do
-$SCRIPTPATH/Quake3-UrT-Ded.x86_64 +set fs_game q3ut4 +set dedicated 2 +set net_port 27960 +set com_hunkmegs 128
+$SCRIPTPATH/Quake3-UrT-Ded.x86_64 +set fs_game q3ut4 +set dedicated 2 +set net_port 27960 +set com_hunkmegs 128 +set ttycon 0
 echo "server crashed on `date`" > last_crash.txt
 done
 
