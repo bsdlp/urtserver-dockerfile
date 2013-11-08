c2bea1b6169cbd6a821daff58e3e312a57e7b989
Jon Chen <bsd@voltaire.sh>
2013-11-08 16:10:36 -0500

    add server start script

---
+++ starturt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPTPATH=$( builtin cd $(dirname $0) ; pwd -P )
+
+while true
+do
+$SCRIPTPATH/Quake3-UrT-Ded.x86_64 +set fs_game q3ut4 +set dedicated 2 +set net_port 27960 +set com_hunkmegs 128 +exec server.cfg
+echo "server crashed on `date`" > last_crash.txt
+done
+
